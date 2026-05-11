346d562f4eb281c0985a3da5dac56dcfa64097b1
carlitroxxx <ca.moil@duocuc.cl>
2026-05-11 01:34:38 -0400

    fix endpoint test

--- front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://100.26.47.236:8080/api/v1/despachos", {
+      .get("/api/v1/despachos", {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
